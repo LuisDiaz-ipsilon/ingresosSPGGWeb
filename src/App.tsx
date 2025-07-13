f41770db9210bbdfb50df5eb86a4be86763f91d2
LuisDiaz-ipsilon <54384617+LuisDiaz-ipsilon@users.noreply.github.com>
2025-07-13 13:32:53 -0600

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MultasPage } from "./pages/MultasPage";
